fcf96f4e1aaef1da3825eadbe975c9d8bc579b66
bernhardtj <31599460+bernhardtj@users.noreply.github.com>
2020-09-14 15:09:22 -0400

    add EDITOR fix

---
+++ 65.editor.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# true
+
+mkdir -p "$HOME/.local/bin"
+
+rm -f "$HOME/.local/bin/emacs"
+
+if ! type emacs >/dev/null 2>/dev/null; then
+    if type flatpak >/dev/null 2>/dev/null; then
+        if flatpak list | grep org.gnu.emacs >/dev/null 2>/dev/null; then
+            printf 'flatpak run org.gnu.emacs "$@"' > "$HOME/.local/bin/emacs"
+        fi
+    fi
+fi
