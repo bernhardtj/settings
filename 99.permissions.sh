564f1101ccbf320b667de980637c57e42965c4f6
bernhardtj <31599460+bernhardtj@users.noreply.github.com>
2020-11-16 15:17:41 -0500

    enforce settings permissions

--- 99.permissions.sh
+++ 99.permissions.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 # true
-chmod +x "$HOME"/.local/bin/*
+find $(dirname ${BASH_SOURCE[0]}) -type f -exec chmod 644 {} \;
+find $(dirname ${BASH_SOURCE[0]}) -type d -exec chmod 755 {} \;
+chmod 755 "$HOME"/.local/bin/* $(dirname ${BASH_SOURCE[0]})/apply
 chmod 600 ~/.ssh/*
+
