994978f1704b87ffb3f391c1094a449635bc3fb6
bernhardtj <31599460+bernhardtj@users.noreply.github.com>
2021-02-08 12:55:12 -0500

    groupinstall

--- install/silverblue.sh
+++ install/silverblue.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-echo 'settings/apply -v && exit' >"$HOME/.zshrc"
-
 pkexec bash <<EOF
 rpm-ostree cancel &&
 rpm-ostree reset &&
 rpm-ostree update &&
 rpm-ostree override remove sudo sudo-python-plugin &&
-rpm-ostree install zsh ImageMagick make gcc g++ &&
+rpm-ostree install zsh ImageMagick make gcc g++ dnf &&
 sed -i 's/\(.*1000.*\)bash/\1zsh/g' /etc/passwd &&
 sed s/^#A/A/g\;s/none/stage/g /usr/etc/rpm-ostreed.conf >/etc/rpm-ostreed.conf &&
 systemctl enable rpm-ostreed-automatic.timer &&
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 EOF
+
+echo 'settings/apply -v && exit' >"$HOME/.zshrc"
 
 reboot
