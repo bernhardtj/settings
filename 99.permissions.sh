9cd93dcae1b4f613100449f002954e18ee63cba7
bernhardtj <31599460+bernhardtj@users.noreply.github.com>
2021-04-17 20:30:45 -0400

    setup for sb mate and sudo is now 'protected packages'

--- 99.permissions.sh
+++ 99.permissions.sh
@@ -5,3 +5,4 @@ find $(dirname ${BASH_SOURCE[0]}) -type f -exec chmod 644 {} \;
 find $(dirname ${BASH_SOURCE[0]}) -type d -exec chmod 755 {} \;
 chmod 755 "$HOME"/.local/bin/* $(dirname ${BASH_SOURCE[0]})/apply
 chmod 600 ~/.ssh/*
+find "$HOME" -maxdepth 1 -type d -empty -exec rmdir {} \;
--- install/silverblue.sh
+++ install/silverblue.sh
@@ -6,7 +6,6 @@ pkexec bash <<EOF
 rpm-ostree cancel &&
 rpm-ostree reset &&
 rpm-ostree update &&
-rpm-ostree override remove sudo sudo-python-plugin &&
 rpm-ostree install zsh ImageMagick make gcc g++ dnf &&
 sed -i 's/\(.*1000.*\)bash/\1zsh/g' /etc/passwd &&
 sed s/^#A/A/g\;s/none/stage/g /usr/etc/rpm-ostreed.conf >/etc/rpm-ostreed.conf &&
