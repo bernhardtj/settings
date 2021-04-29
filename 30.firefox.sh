cc682368cfde84a4116bbab68df445150cded268
bernhardtj <31599460+bernhardtj@users.noreply.github.com>
2021-04-29 12:41:50 -0400

    arkenfox toggle

--- 30.firefox.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# type -p firefox
-rm -rf "$HOME"/{.,.cache/}mozilla
-firefox --headless >/dev/null 2>/dev/null &
-sleep 0.3 && killall firefox
-cd "$HOME"/.mozilla/firefox/*.default-release && curl -sLO https://raw.githubusercontent.com/arkenfox/user.js/master/updater.sh && bash ./updater.sh -s
