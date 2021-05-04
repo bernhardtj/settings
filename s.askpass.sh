44b71996ef954f894c6908f87b6654c07a37d7e8
bernhardtj <31599460+bernhardtj@users.noreply.github.com>
2021-05-04 15:57:07 -0400

    askpass bugfix

--- s.askpass.sh
+++ s.askpass.sh
@@ -1,11 +1,17 @@
 .local/bin/ssh
 #!/bin/bash
-if [[ $1 =~ password: ]]; then
+case "$1" in
+*'password: ')
     PASS="$(sed -n "s/^$1\(.*\)/\1/p" ~/.ssh/pass)"
     [[ $PASS ]] || read -rsp "$1" PASS
     echo $PASS
-else
+    ;;
+*'[fingerprint])? ')
+    echo yes
+    ;;
+*)
     export SSH_ASKPASS_REQUIRE=force
     export SSH_ASKPASS="$(realpath "${BASH_SOURCE[0]}")"
     setsid /bin/ssh "$@"
-fi
+    ;;
+esac
