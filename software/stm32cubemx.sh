70e7ac723ff77fc7698fe7205bee1162d1eb10ff
bernhardtj <31599460+bernhardtj@users.noreply.github.com>
2021-07-07 18:54:39 -0400

    stm32 icons and included jre

--- software/stm32cubemx.sh
+++ software/stm32cubemx.sh
@@ -1,4 +1,4 @@
-PERMALINK=https://www.st.com/content/ccc/resource/technical/software/sw_development_suite/group0/0b/05/f0/25/c7/2b/42/9d/stm32cubemx_v6-1-1/files/stm32cubemx_v6-1-1.zip/jcr:content/translations/en.stm32cubemx_v6-1-1.zip
+PERMALINK=https://www.st.com/content/ccc/resource/technical/software/sw_development_suite/group0/66/75/ec/d9/83/51/4d/d0/stm32cubemx-lin_v6-2-1/files/stm32cubemx-lin_v6-2-1.zip/jcr:content/translations/en.stm32cubemx-lin_v6-2-1.zip
 
 recipe_bin() {
     echo STM32CubeMX
@@ -21,10 +21,23 @@ recipe_install() {
 </AutomatedInstallation>
 EOF
     unzip *.zip
-    chmod +x ./*.linux
-    JAVA_HOME="$(find $HOME/.local/share/JetBrains -type d -name jbr -print -quit)" ./*.linux auto-install.xml
-    mv $HOME/STM32Cube/STM32CubeMX $HOME/STM32Cube/STM32CubeMXelf
-    printf '#!/bin/bash\nJAVA_HOME="$(find $HOME/.local/share/JetBrains -type d -name jbr -print -quit)" " $HOME/STM32Cube/STM32CubeMXelf" "$@"' >$HOME/STM32Cube/STM32CubeMX
-    chmod +x $HOME/STM32Cube/STM32CubeMX
+    ./Setup* auto-install.xml
     ln -sf $HOME/STM32Cube/STM32CubeMX $HOME/.local/bin/STM32CubeMX
+    cat <<EOF >"$HOME/.local/share/applications/stm32cubemx.desktop"
+[Desktop Entry]
+Name=STM32CubeMX
+Icon=STM32CubeMX
+Comment=STM32CubeMX
+Exec=$HOME/STM32Cube/STM32CubeMX
+Version=1.0
+Type=Application
+Categories=Development;IDE;
+Terminal=false
+StartupNotify=true
+EOF
+    identify "$HOME/STM32Cube/help/STM32CubeMX.ico" | while read line; do
+        res="$HOME/.local/share/icons/hicolor/"$(cut -d ' ' -f3 <<<"$line")"/apps"
+        mkdir -p "$res"
+        convert "$(cut -d ' ' -f1 <<<"$line")" "$res/STM32CubeMX.png"
+    done
 }
