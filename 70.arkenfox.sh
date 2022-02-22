ec90901374f71014b46cf365b2266f952203b921
bernhardtj <31599460+bernhardtj@users.noreply.github.com>
2022-02-22 09:48:51 -0500

    menu bar

--- 70.arkenfox.sh
+++ 70.arkenfox.sh
@@ -13,10 +13,11 @@ fi
 
 for profile in "$HOME"/.mozilla/firefox/*.default-release; do
     cat "$HOME/.arkenfox" <(sed -n '/^user_pref/,$p' "${BASH_SOURCE[0]}") > "$profile/user.js"
+    printf '{"chrome://browser/content/browser.xhtml":{"toolbar-menubar":{"autohide":"false"}}}' > "$profile/xulstore.json"
 done
 exit
 
 # overrides
 user_pref("browser.startup.homepage", "https://start.fedoraproject.org/");
 user_pref("browser.startup.page", 1);
-user_pref("browser.uiCustomization.state", "{\"placements\":{\"nav-bar\":[\"urlbar-container\",\"search-container\"]}}");
+user_pref("browser.uiCustomization.state", '{"placements":{"nav-bar":["urlbar-container","search-container"]}}');
