975234f17215e835f39e5bcb1470afd763eee3a9
bernhardtj <31599460+bernhardtj@users.noreply.github.com>
2023-10-24 11:54:06 -0400

    workspace keys

--- 20.gsettings-gnome.sh
+++ 20.gsettings-gnome.sh
@@ -36,3 +36,13 @@ custom-keybindings=['/org/gnome/settings-daemon/plugins/media-keys/custom-keybin
 binding='<Super>Return'
 command='kitty -1'
 name='kitty'
+
+[org/gnome/desktop/wm/keybindings]
+move-to-workspace-1=['<Shift><Super>1']
+move-to-workspace-2=['<Shift><Super>2']
+move-to-workspace-3=['<Shift><Super>3']
+move-to-workspace-4=['<Shift><Super>4']
+switch-to-workspace-1=['<Super>1']
+switch-to-workspace-2=['<Super>2']
+switch-to-workspace-3=['<Super>3']
+switch-to-workspace-4=['<Super>4']
