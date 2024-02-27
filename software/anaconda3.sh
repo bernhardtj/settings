5125b066074e4a32146c0a4e222151c1cc972bc1
bernhardtj <31599460+bernhardtj@users.noreply.github.com>
2024-02-27 17:20:00 -0500

    tl fixes

--- software/anaconda3.sh
+++ software/anaconda3.sh
@@ -4,6 +4,6 @@ recipe_bin() {
 
 recipe_install() {
     curl '-#Lo' anaconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-    bash anaconda.sh -bfp ~/anaconda3
+    echo 'Working...' && bash anaconda.sh -bfp ~/anaconda3 >/dev/null 2>&1
     ln -sf "$HOME/anaconda3/bin/conda" "$HOME/.local/bin/conda"
 }
--- software/texlive.sh
+++ software/texlive.sh
@@ -3,40 +3,39 @@ recipe_bin() {
 }
 
 recipe_install() {
+    local permalink='https://ctan.math.utah.edu/ctan/tex-archive/systems/texlive/tlnet/install-tl-unx.tar.gz'
     mkdir -p ~/.local/share/install-tl
-    curl '-#L' https://ctan.math.utah.edu/ctan/tex-archive/systems/texlive/tlnet/install-tl-unx.tar.gz | tar -xzC ~/.local/share/install-tl/ --strip 1
-    cat >~/.local/bin/install-tl <<<'#!/bin/bash
-if [[ $UID -ne 0 ]]; then
-    echo "Please run this script as root:
+    curl '-#L' "$permalink" | tar -xzC ~/.local/share/install-tl/ --strip 1
+    cat <<EOF >~/.local/bin/install-tl
+#!/bin/bash
 
-sudo $0"
-    exit 1
-fi
 if !(perl -e "use File::Find" 2>&1 >/dev/null); then
     echo "Please install perl-File-Find for this to work."
     exit 1
 fi
-pushd ~/.local/share/install-tl
-./install-tl "$@"
+
+if [[ \$UID -ne 0 ]]; then
+    echo "Please run this script as root:
+
+sudo \$0"
+    exit 1
+fi
+
+pushd $HOME/.local/share/install-tl
+./install-tl "\$@"
 popd
-'
+EOF
     chmod +x ~/.local/bin/install-tl
 }
 
-# https://ctan.math.utah.edu/ctan/tex-archive/systems/texlive/tlnet/install-tl-unx.tar.gz
-# sudo ./install-tl --paper=letter --texdir=/usr/local/texlive/2023
+# example invocation
 #
-# NB
-# this is also packaged in anaconda
+# $ sudo ./install-tl --paper=letter --texdir=/usr/local/texlive/2023
 #
-# !/bin/bash
 #
-# mkdir -p /tmp/acond
 #
-# if [[ ! -f /tmp/acond/anaconda.sh ]]; then
-# curl '-#Lo' /tmp/acond/anaconda.sh \
-# "$(curl '-sL' https://www.anaconda.com/distribution | sed -ne 's@.*\(https:\/\/repo\.anaconda\.com\/archive\/Anaconda3-.*-Linux-x86_64\.sh\)\">64-Bit (x86) Installer.*@\1@p')"
-# #https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-# fi
+# emacs flatpak usage
 #
-# bash /tmp/acond/anaconda.sh -bfp ~/anaconda3
+# $ sudo flatpak override org.gnu.emacs --filesystem=/var/usrlocal
+# $ (setq exec-path (append exec-path '("/var/usrlocal/texlive/2023/bin/x86_64-linux/")))
+#
