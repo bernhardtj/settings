58989d4858598b91fd6c9b9b88ed54de2345084a
bernhardtj <31599460+bernhardtj@users.noreply.github.com>
2024-03-04 10:18:11 -0500

    editor fix

--- 65.editor.sh
+++ 65.editor.sh
@@ -8,7 +8,7 @@ rm -f "$HOME/.local/bin/emacs"
 if ! type emacs >/dev/null 2>/dev/null; then
     if type flatpak >/dev/null 2>/dev/null; then
         if flatpak list | grep org.gnu.emacs >/dev/null 2>/dev/null; then
-            printf 'flatpak run --command=bash org.gnu.emacs -ic "TERM=xterm-256color /app/bin/emacs $*"' >"$HOME/.local/bin/emacs"
+            printf 'flatpak run --command=bash org.gnu.emacs -ic "TERM=xterm-256color /app/bin/emacs-wrapper $*"' >"$HOME/.local/bin/emacs"
         fi
     fi
 fi
