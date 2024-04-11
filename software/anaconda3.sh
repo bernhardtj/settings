8ca5475da26f6e3933359d0e2c2d03376dfc5988
bernhardtj <31599460+bernhardtj@users.noreply.github.com>
2024-04-11 10:38:42 -0400

    pkgs bugfix conda

--- software/anaconda3.sh
+++ software/anaconda3.sh
@@ -4,6 +4,8 @@ recipe_bin() {
 
 recipe_install() {
     curl '-#Lo' anaconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    rm -rf "$HOME/anaconda3"
     echo 'Working...' && bash anaconda.sh -bfp ~/anaconda3 >/dev/null 2>&1
+    rm -rf "$HOME/anaconda3/pkgs"
     ln -sf "$HOME/anaconda3/bin/conda" "$HOME/.local/bin/conda"
 }
