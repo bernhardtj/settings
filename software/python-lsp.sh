c7033357501365a58c86d76be47d927ae9527cb9
bernhardtj <31599460+bernhardtj@users.noreply.github.com>
2024-07-10 15:02:08 -0400

    jedi-langserver

--- software/python-lsp.sh
+++ software/python-lsp.sh
@@ -1,8 +1,10 @@
 recipe_bin() {
     echo pyright
     echo pyright-langserver
+    echo jedi-language-server
 }
 
 recipe_install() {
     npm install -g pyright
+    pipx install -f jedi-language-server
 }
