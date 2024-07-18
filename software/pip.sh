70a70ff0af455e31fd4e66473e9f113dcad4ab72
bernhardtj <31599460+bernhardtj@users.noreply.github.com>
2024-07-18 11:39:03 -0400

    system python

--- software/pip.sh
+++ software/pip.sh
@@ -4,7 +4,7 @@ recipe_bin() {
 
 recipe_install() {
     #python -m ensurepip --upgrade --default-pip --user
-    python -m venv venv
+    /bin/python -m venv venv
     venv/bin/python -m pip install pipx
     venv/bin/pipx install pipx --force
 }
