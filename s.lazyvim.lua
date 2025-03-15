525b81141f2c3d0281543002e690d449d3fdd31f
bernhardtj <31599460+bernhardtj@users.noreply.github.com>
2025-03-15 00:18:43 -0400

    lazyvim -> chadvim

--- s.lazyvim.lua
+++ s.lazyvim.lua
@@ -1,103 +1,26 @@
--- .config/nvim/lua/plugins/custom.lua
-return {
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "tokyonight-night",
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		opts = {
-			servers = {
-				pyright = {},
-				bashls = {},
-				rust_analyzer = {},
-				texlab = {
-					settings = {
-						texlab = {
-							auxDirectory = ".",
-							bibtexFormatter = "texlab",
-							build = {
-								args = { "-X", "compile", "%f", "--synctex", "--keep-logs", "--keep-intermediates" },
-								executable = "tectonic",
-								forwardSearchAfter = true,
-								onSave = true,
-							},
-							chktex = {
-								onEdit = true,
-								onOpenAndSave = true,
-							},
-							diagnosticsDelay = 300,
-							formatterLineLength = 80,
-							forwardSearch = {
-								executable = "evince-synctex",
-								args = { "-f", "%l", "%p", '"code -g %f:%l"' },
-							},
-							latexFormatter = "latexindent",
-							latexindent = {
-								modifyLineBreaks = false,
-							},
-						},
-					},
-				},
-			},
-		},
-	},
-	{
-		"nvimdev/dashboard-nvim",
-		lazy = false, -- As https://github.com/nvimdev/dashboard-nvim/pull/450, dashboard-nvim shouldn't be lazy-loaded to properly handle stdin.
-		opts = function()
-			local logo = string.rep("\n", 8) .. "NVIM " .. tostring(vim.version()) .. "\n\n"
+-- .config/nvim/lua/chadrc.lua
 
-			local opts = {
-				theme = "doom",
-				hide = {
-					-- this is taken care of by lualine
-					-- enabling this messes up the actual laststatus setting after loading a file
-					statusline = false,
-				},
-				config = {
-					header = vim.split(logo, "\n"),
-        -- stylua: ignore
-        center = {
-          { action = 'lua LazyVim.pick()()',                           desc = " Find File",       icon = " ", key = "f" },
-          { action = "ene | startinsert",                              desc = " New File",        icon = " ", key = "n" },
-          { action = 'lua LazyVim.pick("oldfiles")()',                 desc = " Recent Files",    icon = " ", key = "r" },
-          { action = 'lua LazyVim.pick("live_grep")()',                desc = " Find Text",       icon = " ", key = "g" },
-          -- { action = 'lua LazyVim.pick.config_files()()',              desc = " Config",          icon = " ", key = "c" },
-          { action = 'lua require("persistence").load()',              desc = " Restore Session", icon = " ", key = "s" },
-          { action = "LazyExtras",                                     desc = " Package Groups",     icon = " ", key = "x" },
-          { action = "Lazy",                                           desc = " Packages",            icon = "󰒲 ", key = "l" },
-          { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit",            icon = " ", key = "q" },
-        },
-					footer = function()
-						local stats = require("lazy").stats()
-						local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-						return {
-							stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms",
-						}
-					end,
-				},
-			}
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :( 
 
-			for _, button in ipairs(opts.config.center) do
-				button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
-				button.key_format = "  %s"
-			end
+---@type ChadrcConfig
+local M = {}
 
-			-- close Lazy and re-open when the dashboard is ready
-			if vim.o.filetype == "lazy" then
-				vim.cmd.close()
-				vim.api.nvim_create_autocmd("User", {
-					pattern = "DashboardLoaded",
-					callback = function()
-						require("lazy").show()
-					end,
-				})
-			end
+M.base46 = {
+	theme = "tokyodark",
 
-			return opts
-		end,
-	},
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
 }
+
+-- M.nvdash = { load_on_startup = true }
+-- M.ui = {
+--       tabufline = {
+--          lazyload = false
+--      }
+--}
+
+return M
--- s.nvim.lua
+++ s.nvim.lua
@@ -11,41 +11,173 @@ end
 
 EnsurePackage("github.com/folke/lazy.nvim.git")
 
+vim.g.base46_cache = vim.fn.stdpath("data") .. "/base46/"
+vim.g.mapleader = " "
+
 require("lazy").setup({
 	spec = {
-		-- add LazyVim and import its plugins
-		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
-		-- import any extras modules here
-		-- { import = "lazyvim.plugins.extras.lang.typescript" },
-		-- { import = "lazyvim.plugins.extras.lang.json" },
-		-- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-		-- import/override with your plugins
-		{ import = "plugins" },
+		{
+			"NvChad/NvChad",
+			lazy = false,
+			branch = "v2.5",
+			import = "nvchad.plugins",
+		},
+
+		{
+			"stevearc/conform.nvim",
+			-- event = 'BufWritePre', -- uncomment for format on save
+			opts = {
+				formatters_by_ft = {
+					lua = { "stylua" },
+					-- css = { "prettier" },
+					-- html = { "prettier" },
+				},
+
+				-- format_on_save = {
+				--   -- These options will be passed to conform.format()
+				--   timeout_ms = 500,
+				--   lsp_fallback = true,
+				-- },
+			},
+		},
+
+		-- These are some examples, uncomment them if you want to see them work!
+		{
+			"neovim/nvim-lspconfig",
+			config = function()
+				-- load defaults i.e lua_lsp
+				require("nvchad.configs.lspconfig").defaults()
+
+				local lspconfig = require("lspconfig")
+
+				-- EXAMPLE
+				local servers = { "html", "cssls", "pyright", "bashls", "rust_analyzer"}
+				local nvlsp = require("nvchad.configs.lspconfig")
+
+				-- lsps with default config
+				for _, lsp in ipairs(servers) do
+					lspconfig[lsp].setup({
+						on_attach = nvlsp.on_attach,
+						on_init = nvlsp.on_init,
+						capabilities = nvlsp.capabilities,
+					})
+				end
+
+				-- configuring single server, example: typescript
+				lspconfig.texlab.setup({
+					on_attach = nvlsp.on_attach,
+					on_init = nvlsp.on_init,
+					capabilities = nvlsp.capabilities,
+					settings = {
+						texlab = {
+							auxDirectory = ".",
+							bibtexFormatter = "texlab",
+							build = {
+								args = { "-X", "compile", "%f", "--synctex", "--keep-logs", "--keep-intermediates" },
+								executable = "tectonic",
+								forwardSearchAfter = true,
+								onSave = true,
+							},
+							chktex = {
+								onEdit = true,
+								onOpenAndSave = true,
+							},
+							diagnosticsDelay = 300,
+							formatterLineLength = 80,
+							forwardSearch = {
+								executable = "evince-synctex",
+								args = { "-f", "%l", "%p", '"code -g %f:%l"' },
+							},
+							latexFormatter = "latexindent",
+							latexindent = {
+								modifyLineBreaks = false,
+							},
+						},
+					},
+				})
+			end,
+		},
+
+		-- {
+		-- 	"nvim-treesitter/nvim-treesitter",
+		-- 	opts = {
+		-- 		ensure_installed = {
+		-- 			"vim", "lua", "vimdoc",
+		--      "html", "css"
+		-- 		},
+		-- 	},
+		-- }, },
 	},
-	defaults = {
-		-- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-		-- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-		lazy = false,
-		-- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-		-- have outdated releases, which may break your Neovim install.
-		version = false, -- always use the latest git commit
-		-- version = "*", -- try installing the latest stable version for plugins that support semver
+
+	defaults = { lazy = true },
+	install = { colorscheme = { "nvchad" } },
+
+	ui = {
+		icons = {
+			ft = "",
+			lazy = "󰂠 ",
+			loaded = "",
+			not_loaded = "",
+		},
 	},
-	install = { colorscheme = { "tokyonight", "habamax" } },
-	checker = { enabled = false }, -- automatically check for plugin updates
+
 	performance = {
 		rtp = {
-			-- disable some rtp plugins
 			disabled_plugins = {
-				"gzip",
-				-- "matchit",
-				-- "matchparen",
-				-- "netrwPlugin",
-				"tarPlugin",
+				"2html_plugin",
 				"tohtml",
-				"tutor",
+				"getscript",
+				"getscriptPlugin",
+				"gzip",
+				"logipat",
+				"netrw",
+				"netrwPlugin",
+				"netrwSettings",
+				"netrwFileHandlers",
+				"matchit",
+				"tar",
+				"tarPlugin",
+				"rrhelper",
+				"spellfile_plugin",
+				"vimball",
+				"vimballPlugin",
+				"zip",
 				"zipPlugin",
+				"tutor",
+				"rplugin",
+				"syntax",
+				"synmenu",
+				"optwin",
+				"compiler",
+				"bugreport",
+				"ftplugin",
 			},
 		},
 	},
 })
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require("nvchad.options")
+
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+
+require("nvchad.autocmds")
+
+vim.schedule(function()
+	require("nvchad.mappings")
+
+	-- add yours here
+
+	local map = vim.keymap.set
+
+	map("n", ";", ":", { desc = "CMD enter command mode" })
+	map("i", "jk", "<ESC>")
+
+	-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+end)
