bbee2e0e526fa02066667295f6a5c1e7c28d5709
bernhardtj <31599460+bernhardtj@users.noreply.github.com>
2025-11-26 21:49:13 -0500

    fonts update

--- 82.blobs.sh
+++ 82.blobs.sh
@@ -2,6 +2,6 @@
 # true
 
 if [[ ! -e "$HOME"/.local/share/fonts/arial.ttf ]]; then
-    git clone https://github.com/bernhardtj/settings --single-branch -b blobs /tmp/blobs
+    git clone https://github.com/bernhardtj/settings --single-branch --depth 1 -b blobs /tmp/blobs
     cat /tmp/blobs/01.fonts.tar.xz.* | tar xvJC "$HOME"
 fi
