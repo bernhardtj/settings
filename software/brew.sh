cea0c58845a3f8add98ab6c44c9858072bc1f87a
bernhardtj <31599460+bernhardtj@users.noreply.github.com>
2025-12-07 15:13:52 -0500

    brew

--- software/brew.sh
+++ software/brew.sh
@@ -4,4 +4,10 @@ recipe_bin() {
 
 recipe_install() {
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    cat <<'EOF' >"$HOME/.local/bin/brew"
+#!/bin/bash
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+brew "$@"
+EOF
+    chmod +x "$HOME/.local/bin/brew"
 }
