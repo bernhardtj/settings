047e42bd2ba74a84bd75f8c411c046f543e01346
bernhardtj <31599460+bernhardtj@users.noreply.github.com>
2026-02-08 13:27:12 -0500

    fixes

--- s.motd.sh
+++ s.motd.sh
@@ -3,7 +3,7 @@
 
 d30=2592000
 
-d=$(date +%s -d "$(rpm-ostree status | sed -n '/Version/{s/^.*(\(.*\))$/\1/gp;q}')")
+d=$(date -d "$(rpm-ostree status | sed -n '/Version/{s/^.*(\(.*\))$/\1/gp;q}')" +%s)
 
 if [[ $((d + d30)) -lt $(date +%s) ]]; then
     printf "Notice: stale base image @%s\n" "$(date +%D -d "@$d")"
