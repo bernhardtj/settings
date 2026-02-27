fb151f1b48fdb12d11179a7e6945baab81a54a19
bernhardtj <31599460+bernhardtj@users.noreply.github.com>
2026-02-26 22:33:54 -0500

    arkenfox fix

--- 70.arkenfox.sh
+++ 70.arkenfox.sh
@@ -3,7 +3,7 @@
 
 URL=https://raw.githubusercontent.com/arkenfox/user.js/master/user.js
 
-if [[ ! -e $HOME/.mozilla/firefox ]]; then
+if [[ ! -e $HOME/.config/mozilla/firefox ]]; then
     firefox --headless >/dev/null 2>/dev/null &
     pid=$!
     sleep 0.3 && kill -9 $pid 2>/dev/null
@@ -11,7 +11,7 @@ fi
 
 [[ ! -e "$HOME/.arkenfox" ]] && curl -sLo "$HOME/.arkenfox" "$URL"
 
-for profile in "$HOME"/.mozilla/firefox/*.default-release; do
+for profile in "$HOME"/.config/mozilla/firefox/*.default-release; do
     cat "$HOME/.arkenfox" <(sed -n '/^user_pref/,$p' "${BASH_SOURCE[0]}") > "$profile/user.js"
     printf '{"chrome://browser/content/browser.xhtml":{"toolbar-menubar":{"autohide":"false"}}}' > "$profile/xulstore.json"
 done
